5ff406c98e0c6ff64c039c7fe7034a68c58feeed
Brendan <yang.b@husky.neu.edu>
2019-03-27 20:45:37 -0400

    jeopardy api creates a board now

--- lib/jeopardy/jeopardy_api.ex
+++ lib/jeopardy/jeopardy_api.ex
@@ -1,9 +1,43 @@
 defmodule Jeopardy.JeopardyAPI do
+
+  ### november 26, 2001 is the transition to double points
+  @transition elem(DateTime.from_iso8601("2001-11-26T12:00:00Z"), 1)
+  @num_categories 6
+  @num_questions 5
+
   def create_board() do 
     resp = HTTPoison.get!("http://jservice.io/api/random?count=10")
     data = Jason.decode!(resp.body)
-    categories = get_categories(6, data, [])
-    categories
+    categories = get_categories(@num_categories, data, [])
+    Enum.map(categories, &(translate_category_by_id(&1)))
+  end
+
+  def translate_category_by_id(id) do
+    resp = HTTPoison.get!("http://jservice.io/api/category?id="<>Integer.to_string(id))
+    data = Jason.decode!(resp.body)
+    title = data["title"]
+    clues = data["clues"]
+    Map.put(%{}, title, get_random_qset(clues, %{}))
+  end
+
+  def get_random_qset(clues, map) when map_size(map) == @num_questions, do: map
+  def get_random_qset(clues, map) do
+    random_clue = Enum.random(clues)
+    clue_body = %{question: random_clue["question"], answer: random_clue["answer"]}
+    if random_clue["value"] == nil do
+      get_random_qset(clues, Map.put_new(map, Integer.to_string(Enum.random(1..5)*200), clue_body))
+    else
+      get_random_qset(clues, Map.put_new(map, get_clue_value(random_clue), clue_body))
+    end
+  end
+
+  def get_clue_value(clue) do
+    {msg, date_aired, _} = DateTime.from_iso8601(clue["airdate"])
+    if DateTime.compare(date_aired, @transition) == :lt do
+      Integer.to_string(clue["value"] * 2)
+    else
+      Integer.to_string(clue["value"])
+    end
   end
 
   def get_categories(0, body, acc), do: acc
