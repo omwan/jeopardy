5414e8997ad7d54cbc20b55d40693dbe317de415
Olivia Wan <wan.o@husky.neu.edu>
2019-04-03 11:59:45 -0400

    integrating ex_twilio into existing implementation

--- lib/jeopardy_web/controllers/twilio_controller.ex
+++ lib/jeopardy_web/controllers/twilio_controller.ex
@@ -6,7 +6,13 @@ defmodule JeopardyWeb.TwilioController do
   action_fallback JeopardyWeb.FallbackController
 
   def receive(conn, params) do
-    response = TwilioAPI.send_message(params["From"], params["Body"])
+    {:ok, twilio_response} = TwilioAPI.send_message(params["From"], params["Body"])
+    response = %{
+      body: twilio_response.body,
+      date_created: twilio_response.date_created,
+      to: twilio_response.to,
+      from: twilio_response.from
+    }
     conn
     |> put_resp_header("content-type", "application/json; charset=UTF-8")
     |> send_resp(:created, Jason.encode!(response))
