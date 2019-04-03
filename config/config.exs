8370ce11e55716f363204957105c32e8ca7679f6
Brendan <yang.b@husky.neu.edu>
2019-04-03 11:15:48 -0400

    ex_twilio stuff

--- config/config.exs
+++ config/config.exs
@@ -25,6 +25,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Use ex_twilio for talking to Twilio
+config :ex_twilio, 
+  account_sid: System.get_env("TWILIO_ACCOUNT_SID"),
+  auth_token: System.get_env("TWILIO_AUTH_TOKEN")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
