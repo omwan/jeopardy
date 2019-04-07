37a42e7030633928c806c52bb5cdd71e549268bd
koucaco <hwang.ch@husky.neu.edu>
2019-04-06 20:07:08 -0400

    edit so you can only see your own shit

--- assets/js/user/show-user.jsx
+++ assets/js/user/show-user.jsx
@@ -8,18 +8,28 @@ function ShowUser(props) {
   let {id, session, records} = props;
   let edit;
 
-  if (session && session.user_id == id) {
-    edit = <span><Link to={"/users/" + session.user_id + "/edit"}>Edit Account</Link> | </span>;
+  if (!session) {
+    return <div>
+      <h2>{title}</h2>
+      <p>You must be logged in to view a user account.</p>
+    </div>;
+  }
+
+  if (session.user_id != id) {
+    return <div>
+      <h2>{title}</h2>
+      <p>You can only view your own user account.</p>
+    </div>;
   }
 
   return <div className="show-user">
-    <h1>Account info:</h1>
+    <h1>Your Account info:</h1>
     <div className="email">Email: {session.username}</div> 
 
     <div className="row">
         <Records id={id} title="Game History"/>
     </div>
-    {edit}<Link to={"/"} >Back to Lobby</Link>
+     <span><Link to={"/users/" + id + "/edit"}>Edit Account</Link> | </span><Link to={"/"} >Back to Lobby</Link>
 
   </div>
 }
